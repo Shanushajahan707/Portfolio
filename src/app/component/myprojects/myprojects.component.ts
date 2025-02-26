3585b03946a546b3b45b8e22aabe3fcbdcfdf817
Shanushajahan707 <shanushajahan707@gmail.com>
2025-02-26 19:42:01 +0530

    my projects section

--- src/app/component/myprojects/myprojects.component.ts
+++ src/app/component/myprojects/myprojects.component.ts
@@ -7,5 +7,48 @@ import { Component } from '@angular/core';
   styleUrl: './myprojects.component.css'
 })
 export class MyprojectsComponent {
-
+   projects = [
+    {
+        title: "Live Streaming Platform",
+        image: "capturelive.png",
+        description: "A one-to-many live streaming platform using WebRTC, Angular 17, and Node.js.",
+        liveDemo: "https://capturelive-shanushajahan707s-projects.vercel.app/",
+        github: "https://github.com/Shanushajahan707/Live-Stream-Frontend"
+    },
+    {
+        title: "MEAN Auth App",
+        image: "meanauth.png",
+        description: "A secure authentication system with JWT, Angular Router, and Ngrx Toolkit.",
+        liveDemo: "",
+        github: "https://github.com/Shanushajahan707/Mean-Auth-App"
+    },
+    {
+        title: "E-Commerce Platform",
+        image: "ecart.png",
+        description: "A dynamic e-commerce site with OTP authentication and payment integration.",
+        liveDemo: "https://onlineecart.onrender.com",
+        github: "https://github.com/Shanushajahan707/Ecommerce-Ecart"
+    },
+    {
+        title: "Weather App",
+        image: "weather_touch.png",
+        description: "A real-time weather application using OpenWeather API and Angular.",
+        liveDemo: "https://weathertouch-shanushajahan707s-projects.vercel.app/",
+        github: "https://github.com/Shanushajahan707/Weather-App-Frontend"
+    },
+    {
+        title: "Netflix Clone",
+        image: "netflix_clone.png",
+        description: "A Netflix-like streaming platform built using Angular.",
+        liveDemo: "",
+        github: "https://github.com/Shanushajahan707/Netflix-Clone-Angular"
+    },
+    {
+        title: "Welfare Organization System",
+        image: "welfare_organization_mgnt.png",
+        description: "A management system for welfare organizations in Kerala.",
+        liveDemo: "",
+        github: "https://github.com/Shanushajahan707/Welfare_organization_management_system.git"
+    }
+];
 }
