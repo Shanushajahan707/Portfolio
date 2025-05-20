d8060116f4fc814432775a4242bf3fe4bebb78e4
Shanushajahan707 <shanushajahan707@gmail.com>
2025-05-20 15:09:03 +0530

    back 2 top added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'buildwithme';
+
+  showScrollTop: boolean = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.showScrollTop = window.scrollY > 300;
+  }
+
+  scrollToTop(): void {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
   
 ngOnInit() {
   const cursor = document.getElementById('custom-cursor');
