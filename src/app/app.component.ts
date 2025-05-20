1d8d66849c60660fb1d8bc49eb542caa5dfeac1d
Shanushajahan707 <shanushajahan707@gmail.com>
2025-05-20 14:42:18 +0530

    ui updated , resume updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +6,38 @@ import { Component } from '@angular/core';
   standalone: false,
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'portfolio';
+export class AppComponent implements OnInit {
+  title = 'buildwithme';
+  
+ngOnInit() {
+  const cursor = document.getElementById('custom-cursor');
+
+  let mouseX = 0;
+  let mouseY = 0;
+  let currentX = 0;
+  let currentY = 0;
+  const speed = 0.1; // Lower = slower
+
+  document.addEventListener('mousemove', (e) => {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+  });
+
+  function animate() {
+    currentX += (mouseX - currentX) * speed;
+    currentY += (mouseY - currentY) * speed;
+
+    if (cursor) {
+      cursor.style.left = `${currentX}px`;
+      cursor.style.top = `${currentY}px`;
+    }
+
+    requestAnimationFrame(animate);
+  }
+
+  animate();
+}
+
+
+
 }
--- src/app/component/myprojects/myprojects.component.ts
+++ src/app/component/myprojects/myprojects.component.ts
@@ -4,51 +4,64 @@ import { Component } from '@angular/core';
   selector: 'app-myprojects',
   standalone: false,
   templateUrl: './myprojects.component.html',
-  styleUrl: './myprojects.component.css'
+  styleUrl: './myprojects.component.css',
 })
 export class MyprojectsComponent {
-   projects = [
+  projects = [
     {
-        title: "Live Streaming Platform",
-        image: "capturelive.png",
-        description: "A one-to-many live streaming platform using WebRTC, Angular 17, and Node.js.",
-        liveDemo: "https://capture-live.vercel.app/",
-        github: "https://github.com/Shanushajahan707/Live-Stream-Frontend"
+      title: 'Belles Avenue',
+      image: 'bellesavenue.png',
+      description:
+        'Belles Avenue, a fashion and beauty shop. Built with Angular, Tailwind CSS, and integrated with a digital billing system.',
+      liveDemo: 'https://bellesavenue.vercel.app',
+      github: 'https://github.com/Shanushajahan707/digitalbilling-frontend',
     },
     {
-        title: "MEAN Auth App",
-        image: "meanauth.png",
-        description: "A secure authentication system with JWT, Angular Router, and Ngrx Toolkit.",
-        liveDemo: "",
-        github: "https://github.com/Shanushajahan707/Mean-Auth-App"
+      title: 'Live Streaming Platform',
+      image: 'capturelive.png',
+      description:
+        'A one-to-many live streaming platform using WebRTC, Angular 17, and Node.js.',
+      liveDemo: 'https://capture-live.vercel.app/',
+      github: 'https://github.com/Shanushajahan707/Live-Stream-Frontend',
     },
     {
-        title: "E-Commerce Platform",
-        image: "ecart.png",
-        description: "A dynamic e-commerce site with OTP authentication and payment integration.",
-        liveDemo: "https://onlineecart.onrender.com",
-        github: "https://github.com/Shanushajahan707/Ecommerce-Ecart"
+      title: 'MEAN Auth App',
+      image: 'meanauth.png',
+      description:
+        'A secure authentication system with JWT, Angular Router, and Ngrx Toolkit.',
+      liveDemo: '',
+      github: 'https://github.com/Shanushajahan707/Mean-Auth-App',
     },
     {
-        title: "Weather App",
-        image: "weather_touch.png",
-        description: "A real-time weather application using OpenWeather API and Angular.",
-        liveDemo: "https://weather-touch.vercel.app/login",
-        github: "https://github.com/Shanushajahan707/Weather-App-Frontend"
+      title: 'E-Commerce Platform',
+      image: 'ecart.png',
+      description:
+        'A dynamic e-commerce site with OTP authentication and payment integration.',
+      liveDemo: 'https://onlineecart.onrender.com',
+      github: 'https://github.com/Shanushajahan707/Ecommerce-Ecart',
     },
     {
-        title: "Netflix Clone",
-        image: "netflix_clone.png",
-        description: "A Netflix-like streaming platform built using Angular.",
-        liveDemo: "",
-        github: "https://github.com/Shanushajahan707/Netflix-Clone-Angular"
+      title: 'Weather App',
+      image: 'weather_touch.png',
+      description:
+        'A real-time weather application using OpenWeather API and Angular.',
+      liveDemo: 'https://weather-touch.vercel.app/login',
+      github: 'https://github.com/Shanushajahan707/Weather-App-Frontend',
     },
     {
-        title: "Welfare Organization System",
-        image: "welfare_organization_mgnt.png",
-        description: "A management system for welfare organizations in Kerala.",
-        liveDemo: "",
-        github: "https://github.com/Shanushajahan707/Welfare_organization_management_system.git"
-    }
-];
+      title: 'Netflix Clone',
+      image: 'netflix_clone.png',
+      description: 'A Netflix-like streaming platform built using Angular.',
+      liveDemo: '',
+      github: 'https://github.com/Shanushajahan707/Netflix-Clone-Angular',
+    },
+    {
+      title: 'Welfare Organization System',
+      image: 'welfare_organization_mgnt.png',
+      description: 'A management system for welfare organizations in Kerala.',
+      liveDemo: '',
+      github:
+        'https://github.com/Shanushajahan707/Welfare_organization_management_system.git',
+    },
+  ];
 }
